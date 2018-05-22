1e23177bbae9762297eb636f0399d143f11d3d64
TempUserWenly <39433774+TempUserWenly@users.noreply.github.com>
2018-05-22 23:21:16 +0800

    Update Welcome plugin.user.js
    updated ver.

--- Welcome plugin.user.js
+++ Welcome plugin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Welcome plugin
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://iirose.com/messages.html
